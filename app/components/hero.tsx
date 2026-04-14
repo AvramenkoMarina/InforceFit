5b75e0ebd07446d57317d32e329fabe9f0f80d1b
Maryna Avramenko <maryna.av@inforce.digital>
2026-04-14 15:54:20 +0300

    change copy

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
         <div className="absolute z-10 md:top-[14%] md:w-[33%] md:left-37.5 w-full px-8 pt-8">
           <div className="w-[90%]">
             <p className="font-sans font-bold text-[14px] md:text-[16px] whitespace-nowrap">
-              НАЙБІЛЬША БАЗА УКРАЇНСЬКИХ СТРАВ!
+              НАЙБІЛЬША БАЗА УКРАЇНСЬКИХ СТРАВ
             </p>
 
             <h1 className="mt-2 text-[38px] leading-12 md:text-[54px] md:leading-16 font-bold font-sans whitespace-nowrap">
